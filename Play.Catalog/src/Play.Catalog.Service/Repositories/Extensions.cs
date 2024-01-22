6d77a2739633d986d9c4b748c2fd45998e1246a3
Vinícius Martins <viniciusmv91@gmail.com>
2024-01-21 21:11:47 -0300

    Refactoring MongoDB registration into extension methods

---
+++ Play.Catalog/src/Play.Catalog.Service/Repositories/Extensions.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using MongoDB.Bson.Serialization.Serializers;
+using MongoDB.Bson.Serialization;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using Play.Catalog.Service.Entities;
+using Play.Catalog.Service.Settings;
+
+namespace Play.Catalog.Service.Repositories;
+
+public static class Extensions
+{
+    public static IServiceCollection AddMongo(this IServiceCollection services)
+    {
+        BsonSerializer.RegisterSerializer(new GuidSerializer(BsonType.String));
+        BsonSerializer.RegisterSerializer(new DateTimeOffsetSerializer(BsonType.String));
+
+        services.AddSingleton(sp =>
+        {
+            var configuration = sp.GetService<IConfiguration>();
+            var serviceSettings = configuration.GetSection(nameof(ServiceSettings)).Get<ServiceSettings>();
+            var mongoDbSettings = configuration.GetSection(nameof(MongoDbSettings)).Get<MongoDbSettings>();
+            var mongoClient = new MongoClient(mongoDbSettings.ConnectionString);
+            return mongoClient.GetDatabase(serviceSettings.ServiceName);
+        });
+
+        return services;
+    }
+
+    public static IServiceCollection AddMongoRepository<T>(this IServiceCollection services, string collectionName) 
+        where T : IEntity
+    {
+        services.AddSingleton<IRepository<T>>(sp =>
+        {
+            var database = sp.GetService<IMongoDatabase>();
+            return new MongoRepository<T>(database, collectionName);
+        });
+
+        return services;
+    }
+}
